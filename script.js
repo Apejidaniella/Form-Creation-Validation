ba74b94ba47c6f586288522c221b36172a27573b
Daniella <apejidaniella@gmail.com>
2025-08-02 23:50:38 +0100

    added changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (event) => {
     const form = document.getElementById("registration-form") 
 
     const feedbackDiv = document.
@@ -46,8 +46,6 @@ document.addEventListener("DOMContentLoaded", () => {
             const error = messages.join("<br>")
             feedbackDiv.innerHTML = error
              feedbackDiv.style.color = "#dc3545"
-        }
-        
+        } 
     })
-
  })
